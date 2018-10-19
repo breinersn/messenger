d3c9e002faa4cd3404a0bebf34dbc830e4bd68f0
rootkits <rootkits@localhost.localdomain>
2018-10-19 19:58:01 -0400

    exportar usuarios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  friends: User[] = [];
+
+  constructor() {
+    const usuario1: User = {
+      nick: 'breiner',
+      subNick: 'suarezn',
+      age: 25,
+      email: 'wando08@hotmail.com',
+      friend: true,
+      uid: 1
+    };
+    const usuario2: User = {
+      nick: 'breiner',
+      subNick: 'suarezn',
+      age: 25,
+      email: 'wando08@hotmail.com',
+      friend: true,
+      uid: 2
+    };
+    const usuario3: User = {
+      nick: 'breiner',
+      subNick: 'suarezn',
+      age: 25,
+      email: 'wando08@hotmail.com',
+      friend: true,
+      uid: 3
+    };
+    const usuario4: User = {
+      nick: 'breiner',
+      subNick: 'suarezn',
+      age: 25,
+      email: 'wando08@hotmail.com',
+      friend: true,
+      uid: 4
+    };
+    const usuario5: User = {
+      nick: 'breiner',
+      subNick: 'suarezn',
+      age: 25,
+      email: 'wando08@hotmail.com',
+      friend: true,
+      uid: 5
+    };
+    this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
+  }
 }
